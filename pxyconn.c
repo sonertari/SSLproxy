12ecc96648385cd3ed81c574e27f9571dc790fb6
Soner Tari <sonertari@gmail.com>
2018-10-30 12:42:52 +0300

    Assume co-ownership of refactored and new pxy and proto source files by adding copyright line below the original copyright line

--- pxyconn.c
+++ pxyconn.c
@@ -3,6 +3,7 @@
  * https://www.roe.ch/SSLsplit
  *
  * Copyright (c) 2009-2018, Daniel Roethlisberger <daniel@roe.ch>.
+ * Copyright (c) 2018, Soner Tari <sonertari@gmail.com>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
