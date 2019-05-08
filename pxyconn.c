adee22db098969afbcde88f3ea9205588d97cf8f
Soner Tari <sonertari@gmail.com>
2019-05-08 21:47:16 +0300

    Fix FreeBSD support, pr #13 by @5u623l20
    FreeBSD does not have getdtablecount() and needs netinet/in.h

--- pxyconn.c
+++ pxyconn.c
@@ -938,7 +938,7 @@ pxy_try_remove_sslproxy_header(pxy_conn_child_ctx_t *ctx, unsigned char *packet,
 	}
 }
 
-#ifdef __APPLE__
+#if defined(__FreeBSD__) || defined(__APPLE__)
 #define getdtablecount() 0
 
 /*
--- pxyconn.h
+++ pxyconn.h
@@ -30,6 +30,10 @@
 #ifndef PXYCONN_H
 #define PXYCONN_H
 
+#if defined(__FreeBSD__) || defined(__DragonFly__)
+#include <netinet/in.h>
+#endif
+
 #include "proxy.h"
 #include "opts.h"
 #include "attrib.h"
