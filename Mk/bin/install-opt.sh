9ac5a93823e383f061be750506780a9dee18bac5
Soner Tari <sonertari@gmail.com>
2020-04-04 18:18:01 +0300

    Fix testproxy e2e tests for older versions of openssl and libressl
    OpenSSL 0.9.8zh and 1.0.0s do not support TLSv11.
    LibreSSL 2.2.7 uses other cipher names too.
    LibreSSL 2.7.4 (since 2.3.0) does not support TLSv10.

--- Mk/bin/install-opt.sh
+++ Mk/bin/install-opt.sh
@@ -8,12 +8,18 @@ if [ -z "$EVENT" ]; then
 	exit 1
 fi
 
+# TLSv1 == TLSv10 == SSLv3
+NO_TLS10= no
+NO_TLS11= no
+
 case "$SSL" in
 openssl-0.9.*)
 	SSLURL=https://www.openssl.org/source/old/0.9.x/$SSL.tar.gz
+	NO_TLS11= yes
 	;;
 openssl-1.0.0*)
 	SSLURL=https://www.openssl.org/source/old/1.0.0/$SSL.tar.gz
+	NO_TLS11= yes
 	;;
 openssl-1.0.1*)
 	SSLURL=https://www.openssl.org/source/old/1.0.1/$SSL.tar.gz
@@ -30,6 +36,10 @@ openssl-1.1.1*)
 openssl-*)
 	SSLURL=https://www.openssl.org/source/$SSL.tar.gz
 	;;
+libressl-2.7.4*)
+	SSLURL=http://ftp.fau.de/pub/OpenBSD/LibreSSL/$SSL.tar.gz
+	NO_TLS10= yes
+	;;
 libressl-*)
 	#SSLURL=https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/$SSL.tar.gz
 	SSLURL=http://ftp.fau.de/pub/OpenBSD/LibreSSL/$SSL.tar.gz
@@ -38,6 +48,8 @@ libressl-*)
 	exit 1
 	;;
 esac
+
+export NO_TLS11
 
 case "$EVENT" in
 libevent-2.1.8)
